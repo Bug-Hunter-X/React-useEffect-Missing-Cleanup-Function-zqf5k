0ab8f0ab18001ef4552be104af4253d77566d660
BugHunter-X <askzenai@gmail.com>
2025-02-02 18:52:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    // Cleanup function: clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>Count: {count}</div>;
+}
+```
